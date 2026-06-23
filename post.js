7054490d0a4c7fa245e8d76ffbd3e93cb5aa3dcb
sukail-123 <sukailmohamed0@gmail.com>
2026-06-23 11:43:45 +0530

    Create post.js

---
+++ post.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const PostSchema =
+new mongoose.Schema({
+
+    title:String,
+    content:String,
+    image:String,
+
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+
+});
+
+module.exports =
+mongoose.model("Post",PostSchema);
